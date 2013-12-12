5aef72020ebab3a09fd648e5c698d0241eabf7fb
FlinGit <florian.ingenpass@gmx.de>
2013-12-12 15:50:59 +0100

    works without restart function yet

---
+++ 71_experiments/Breakout1/build/classes-generated/nl/fontys/sevenlo/games/breakout/Bundle.java
@@ -0,0 +1,27 @@
+package nl.fontys.sevenlo.games.breakout;
+/** Localizable strings for {@link nl.fontys.sevenlo.games.breakout}. */
+@javax.annotation.Generated(value="org.netbeans.modules.openide.util.NbBundleProcessor")
+class Bundle {
+    /**
+     * @return <i>BreakoutRun</i>
+     * @see BreakoutRunTopComponent
+     */
+    static String CTL_BreakoutRunAction() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_BreakoutRunAction");
+    }
+    /**
+     * @return <i>BreakoutRun Window</i>
+     * @see BreakoutRunTopComponent
+     */
+    static String CTL_BreakoutRunTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_BreakoutRunTopComponent");
+    }
+    /**
+     * @return <i>This is a BreakoutRun window</i>
+     * @see BreakoutRunTopComponent
+     */
+    static String HINT_BreakoutRunTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "HINT_BreakoutRunTopComponent");
+    }
+    private void Bundle() {}
+}
