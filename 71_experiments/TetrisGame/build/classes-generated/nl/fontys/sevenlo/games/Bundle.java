8c4bd24bf2ebdb2e21fcd2b0c957effd6491f4e9
Synisse <stefan-krueger@hotmail.de>
2013-12-12 14:15:05 +0100

    initial import

---
+++ 71_experiments/TetrisGame/build/classes-generated/nl/fontys/sevenlo/games/Bundle.java
@@ -0,0 +1,27 @@
+package nl.fontys.sevenlo.games;
+/** Localizable strings for {@link nl.fontys.sevenlo.games}. */
+@javax.annotation.Generated(value="org.netbeans.modules.openide.util.NbBundleProcessor")
+class Bundle {
+    /**
+     * @return <i>Tetris</i>
+     * @see TetrisTopComponent
+     */
+    static String CTL_TetrisAction() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_TetrisAction");
+    }
+    /**
+     * @return <i>Tetris Window</i>
+     * @see TetrisTopComponent
+     */
+    static String CTL_TetrisTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_TetrisTopComponent");
+    }
+    /**
+     * @return <i>This is a Tetris window</i>
+     * @see TetrisTopComponent
+     */
+    static String HINT_TetrisTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "HINT_TetrisTopComponent");
+    }
+    private void Bundle() {}
+}
